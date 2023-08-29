ff349168e3af402c0d1a25fe6fb92b343bab3d81
suplimo <119318755+suplimo@users.noreply.github.com>
2023-08-28 22:37:27 -0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="PT-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="index.html">
+</head>
+<body>
+<div class="login-container">
+        
+        <form action="process-login.php" method="post">
+            <label for="username">e-mail institucional</label>
+            <input type="text" id="username" name="username" required>
+
+            <label for="password">Senha:</label>
+            <input type="password" id="password" name="password" required>
+
+            <button type="submit">Avançar</button>
+        </form>
+    </div> 
+
+    
+</body>
+</html>
